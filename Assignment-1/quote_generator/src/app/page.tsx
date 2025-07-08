b1f20bcaa59e7ceb23f89e4acafff5a51fd4dd41
Haseeb Siddique <haseebsiddique825@gmail.com>
2025-07-09 02:05:20 +0500

    Update page.tsx

--- Assignment-1/quote_generator/src/app/page.tsx
+++ Assignment-1/quote_generator/src/app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
   }
 
   return (
-    <main className="h-screen flex flex-col md:flex-row relative">
+    <main className="md:h-screen flex flex-col md:flex-row relative">
       {/* Animated Background */}
       <div className="absolute inset-0 bg-gradient-to-br from-gray-950 via-gray-900 to-zinc-800">
         <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=%2260%22 height=%2260%22 viewBox=%220 0 60 60%22 xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cg fill=%22none%22 fill-rule=%22evenodd%22%3E%3Cg fill=%22%239C92AC%22 fill-opacity=%220.05%22%3E%3Ccircle cx=%2230%22 cy=%2230%22 r=%222%22/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-40"></div>
